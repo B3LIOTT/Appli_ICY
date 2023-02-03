1fe70c1406efcb3f0f1f49642a9ff37ddc54752a
b3liott <eliott.georges8@gmail.com>
2023-02-03 23:11:44 +0100

    Many modifications

--- lib/Objects/CoursRepo.dart
+++ lib/Objects/CoursRepo.dart
@@ -7,9 +7,9 @@ class CoursRepo {
   List<CoursWidget> widget_list = <CoursWidget>[];
 
   CoursRepo(){
-    widget_list.add(CoursWidget(Section(1, "testmodule", "testmatiere", "")));
+    widget_list.add(CoursWidget(Section(1, "testmodule", "testmatiere", "assets/images/image.png")));
     for(int i = 0; i < 10; i++){
-      widget_list.add(CoursWidget(Section(i%4 + 1, "Module", "Matiere", "")));
+      widget_list.add(CoursWidget(Section(i%4 + 1, "Module", "Matiere", "assets/images/image.png")));
     }
   }
 
