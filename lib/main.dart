1fb3563c8710b5b6abd1a6821fe094023ab17f1a
b3liott <eliott.georges8@gmail.com>
2023-02-04 14:57:58 +0100

    force portrait display

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,15 @@ import 'package:app_icy/firebase_options.dart';
 import 'package:app_icy/home/home.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown
+  ]);
 
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
